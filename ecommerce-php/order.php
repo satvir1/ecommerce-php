82684c9f7d23b413018893f417cc7eb1e3263c9f
inderjeet1215 <43214103+inderjeet1215@users.noreply.github.com>
2018-11-03 15:35:11 -0400

    Add files via upload

---
+++ ecommerce-php/order.php
@@ -0,0 +1,21 @@
+<?php 
+include 'require.php';
+/* Objects*/
+$product = new Products();
+$order = new Orders();
+$cart = new Cart([  'cartMaxItem' => 0, 'itemMaxQuantity' => 99, 'useCookie'=> false, ]);
+/* Objects*/
+
+$cartItems = $cart->getItems();
+
+if (!empty($cartItems)) {
+	
+	$cart_total =  $cart->getAttributeTotal('price');
+	$shipping_cost =  $cart->getAttributeTotal('shipping_cost');
+	$pay_amount = $shipping_cost+$cart_total;
+	$order->AddOrder($cartItems,$pay_amount, 0);
+	$cart->clear();
+	require 'order_view.php';
+}else{
+	header("Location: ".BASE_URL);	
+}
---
+++ ecommerce-php/order_view.php
@@ -0,0 +1,8 @@
+<?php include 'header.php';?>
+<!-- Sidebar ================================================== -->
+<?php require 'sidebar.php'; ?>
+<!-- Sidebar end=============================================== -->
+	<div class="span9">
+		<h4>Thanks you for your order</h4>
+		 </div>
+<?php include 'footer.php'; ?>
